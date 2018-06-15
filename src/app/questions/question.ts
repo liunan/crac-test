8137a987dc8f8d4709cf0ea24560f0c4f79a6630
liunan <liunan@msn.com>
2018-06-15 14:28:43 +0800

    initial commit

---
+++ src/app/questions/question.ts
@@ -0,0 +1,6 @@
+export class Question{    
+    Q:string;//问题
+    C:string[];//答案选项
+    //A:正确答案
+    //U:用户输入答案
+}
---
+++ src/app/questions/questions.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Question } from './question';
+
+import { QuestionRepoA } from './repoA';
+import { AsyncScheduler } from 'rxjs/internal/scheduler/AsyncScheduler';
+
+@Component({
+  selector: 'app-questions',
+  templateUrl: './questions.component.html',
+  styleUrls: ['./questions.component.css']
+})
+export class QuestionsComponent implements OnInit {
+
+  
+  questions = QuestionRepoA;
+  commited:boolean;
+  done:number;
+  score:number;
+  
+  constructor() {
+    this.commited = false;
+    this.done = 0;
+    this.score = 0;
+
+   }
+
+  ngOnInit() {        
+    while(this.filtered.length<4)
+    {
+      let idx =Math.floor(Math.random()*(this.questions.length+1));
+      let item = this.questions[idx];
+      item.A = item.C[0];//正确答案
+
+      let newPos = Math.floor(Math.random()*4);//[0,1,2,3]
+      item.C[0] = item.C[newPos];
+      item.C[newPos] = item.A;
+
+      //console.log(idx);
+      this.filtered.push(this.questions[idx]);
+    }
+  }
+
+  checkAnswer(answer:string,idx:number):void{
+    if(this.filtered[idx].U == undefined)
+      ++this.done;
+
+    console.log(`${idx}:${answer}`);
+    this.filtered[idx].U = answer;  
+  }
+
+  commit():void{
+    this.score=0;
+    for(let item in this.filtered)
+    {  
+      console.log(item);    
+      if(this.filtered[item].A === this.filtered[item].U)
+        ++this.score;
+    }
+
+    this.commited = true;
+
+    
+  }
+
+
+  
+
+  filtered:Question[]=[];
+}
